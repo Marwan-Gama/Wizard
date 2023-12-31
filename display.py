78cbf70eac21fa957bf6fa48f871eb01db348fbf
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2023-12-31 19:25:23 +0200

    create show function that print the details of phase

--- display.py
+++ display.py
@@ -2,8 +2,6 @@ from colorama import Fore, Style
 from tabulate import tabulate
 
 
-
-
 def display_summary(details):
     summary_table = [
         [Fore.CYAN + Style.BRIGHT + "Phase 1", Fore.CYAN + Style.BRIGHT + "Phase 2",
@@ -23,3 +21,18 @@ def display_summary(details):
     print(tabulate(summary_table, headers="firstrow", tablefmt="pretty"))
     print("\n" + Fore.MAGENTA + Style.BRIGHT + "Thank you for registering!" + Style.RESET_ALL)
 
+
+
+def show_phase(num_phase,details):
+    phase_functions = {
+        1: lambda: print_items(1,details, 0, 3),
+        2: lambda: print_items(2,details, 3, 6),
+        3: lambda: print_items(3,details, 6, 8)
+    }
+    phase_functions.get(num_phase, lambda: print("Invalid phase number"))()
+
+
+def print_items(num_phase,details, start, end):
+    print(f'{Fore.GREEN}Phase {num_phase} items:\n')
+    for key, value in list(details.items())[start:end]:
+        print(f'{Fore.BLUE}{key}:{Fore.GREEN} {value}{Style.RESET_ALL}')
