8837b8ab9e89d68854adb7beae66af2935522ea9
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2024-01-01 15:38:38 +0200

    Add Reset button that clears everything and redirects to the beginnin

--- display.py
+++ display.py
@@ -1,38 +1,49 @@
 from colorama import Fore, Style
 from tabulate import tabulate
 
+rest=False
 
 def display_summary(details):
-    summary_table = [
-        [Fore.CYAN + Style.BRIGHT + "Phase 1", Fore.CYAN + Style.BRIGHT + "Phase 2",
-         Fore.CYAN + Style.BRIGHT + "Phase 3"],
-        [f"{Fore.YELLOW}Full Name{Style.RESET_ALL} - {details['Name']}",
-         f"{Fore.YELLOW}City{Style.RESET_ALL} - {details['City']}",
-         f"{Fore.YELLOW}Social Media{Style.RESET_ALL} - {details['Social Media']}"],
-        [f"{Fore.YELLOW}Email{Style.RESET_ALL} - {details['Email']}",
-         f"{Fore.YELLOW}Street{Style.RESET_ALL} - {details['Street']}",
-         f"{Fore.YELLOW}Hobbies{Style.RESET_ALL} - {', '.join(details['Hobbies']) if details['Hobbies'] else '-'}"],
-        [f"{Fore.YELLOW}Birth Date{Style.RESET_ALL} - {details['birth_date']}",
-         f"{Fore.YELLOW}Number{Style.RESET_ALL} - {details['Number'] if details['Number'] else '-'}",
-         ""]
-    ]
+    global rest
+    show_phase(1, details)
+    show_phase(2, details)
+    show_phase(3, details)
+    show_phase(4, details)
 
-    print("\n" + Fore.CYAN + "Summary:" + Style.RESET_ALL)
-    print(tabulate(summary_table, headers="firstrow", tablefmt="pretty"))
-    print("\n" + Fore.MAGENTA + Style.BRIGHT + "Thank you for registering!" + Style.RESET_ALL)
-
+    rest_button=input(f"{Fore.YELLOW} \nenter 1 if you want to rest:")
+    if rest_button=="1":
+        rest_data(details)
+    else:
+        print("Invalid number")
 
 
 def show_phase(num_phase,details):
     phase_functions = {
         1: lambda: print_items(1,details, 0, 3),
         2: lambda: print_items(2,details, 3, 6),
-        3: lambda: print_items(3,details, 6, 8)
+        3: lambda: print_items(3,details, 6, 8),
+        4: lambda: print_items(4,details, 8, 11)
     }
     phase_functions.get(num_phase, lambda: print("Invalid phase number"))()
 
 
 def print_items(num_phase,details, start, end):
-    print(f'{Fore.GREEN}Phase {num_phase} items:\n')
+    table_data = []
     for key, value in list(details.items())[start:end]:
-        print(f'{Fore.BLUE}{key}:{Fore.GREEN} {value}{Style.RESET_ALL}')
+        table_data.append([f'{Fore.BLUE}{key}:{Fore.LIGHTRED_EX}', value])
+
+    table = tabulate(table_data, headers=["Item", "Value"], tablefmt="grid", colalign=("left", "right"))
+    print(f'{Fore.GREEN}Phase {num_phase} items:\n')
+    print(table)
+    print(Style.RESET_ALL)
+
+def get_rest():
+    return rest
+
+def rest_data(details):
+    global rest
+    for k,v in details.items():
+        details[k]=None
+    rest=True
+
+
